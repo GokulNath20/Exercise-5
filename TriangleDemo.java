682b947f1ddd44e2acb6ec372876a21c7ba10104
GokulNath20 <61192087+GokulNath20@users.noreply.github.com>
2020-04-10 15:04:53 +0800

    Add files via upload

---
+++ TriangleDemo.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class TriangleDemo {
+    public static void main(String[] args)
+    {
+        //create a Scanner object
+        Scanner gk = new Scanner(System.in);
+
+        // Create a Triangle object.
+        Triangle triangle = new Triangle();
+
+        // Prompt user to input value for height and base
+        System.out.print("Please enter the height of the triangle: ");
+        double height = gk.nextDouble();
+        System.out.print("Please enter the height of the triangle: ");
+        double base = gk.nextDouble();
+
+        //Set the height and base (use mutator)
+        triangle.setHeight(height);
+        triangle.setBase(base);
+
+        // Display the height, base and area (use accessor)
+        System.out.println("The pyramid's height is " + triangle.getHeight());
+        System.out.println("The pyramid's base is " + triangle.getBase());
+        System.out.println("The pyramid's area is "+ triangle.getArea());
+    }
+
+}
